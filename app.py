303c47b0b20cc75ecf107edb7738e976b5252f3e
maggie3000 <72889935+maggie3000@users.noreply.github.com>
2020-12-10 14:14:04 -0800

    added localhost

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template,redirect,url_for
 #create a Flask instance
-import data
 
 app = Flask(__name__)
 
 #connects default URL of server to render home.html
 @app.route('/')
 def home_route():
-    return render_template("home.html", title = data.setup())
+    return render_template("home.html")
     
 if __name__ == "__main__":
-  app.run(port='3000', host='0.0.0.0')
+  app.run(port='80', host='127.0.0.1')
