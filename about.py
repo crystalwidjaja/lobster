e9c20e885d353245413766c3aac21cbc0ca4911a
crystalwidjaja <pansypig@gmail.com>
2020-12-20 17:44:06 -0800

    a;)

---
+++ about.py
@@ -0,0 +1,35 @@
+def ridhima():
+    name = "Ridhima Inukurti"
+    image = "ridhima.jpg"
+    birthday = "January 10"
+    funfact = "I love to play volleyball and draw in my free time."
+    sign = "Capricorn"
+    profile = {"name": name, "image": image, "birthday": birthday, "funfact": funfact, "sign": sign}
+    return profile
+
+def sriya():
+    name = "Sriya Chilla"
+    image = "sriya.jpg"
+    birthday = "August 29"
+    funfact = "I’m david dobrik’s biggest fan!"
+    sign = "Virgo"
+    profile = {"name": name,  "image": image, "birthday": birthday, "funfact": funfact, "sign": sign}
+    return profile
+
+def crystal():
+    name = "Crystal Widjaja"
+    image = "crystal.JPEG"
+    birthday = "January 7"
+    funfact = "I can play four instruments - piano, guitar, ukulele, and violin."
+    sign = "Capricorn"
+    profile = {"name": name,  "image": image, "birthday": birthday, "funfact": funfact, "sign": sign}
+    return profile
+
+def maggie():
+    name = "Maggie Killada"
+    image = "maggie.jpg"
+    birthday = "August 3"
+    funfact = "I love to take pictures and bake in my free time."
+    sign = "Leo"
+    profile = {"name": name,  "image": image, "birthday": birthday, "funfact": funfact, "sign": sign}
+    return profile
