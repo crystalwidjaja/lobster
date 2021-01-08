491d61729ad1ba30c71e0a81f084bf46b5742e76
ridhimainukurti <45546576+ridhimainukurti@users.noreply.github.com>
2021-01-07 21:39:46 -0800

    art piece information (backend)

--- artpiece.py
+++ artpiece.py
@@ -1,9 +1,9 @@
-import json
 import requests
+import sqlite3 as sl
 
 url = "https://openaccess-api.clevelandart.org/api/artworks/"
 
-payload={"skip": "2", "limit": "10"}
+payload = {"skip": "2", "limit": "10"}
 response = requests.get(url, params=payload)
 
 responseJsonObj = response.json()
@@ -13,6 +13,7 @@ recordedList = []
 for data in dataList:
     title = data['title']
     creationDate = data['creation_date']
+    culture = data['culture']
     authorList = []
     creators = data['creators']
     for creator in creators:
@@ -20,8 +21,29 @@ for data in dataList:
         biography = creator['biography']
         authorDict = dict({"author": author, "biography": biography})
         authorList.append(authorDict)
-    recordedList.append(dict({"title": title, 'creationDate': creationDate, 'creators': authorList}))
+    recordedList.append(
+        dict({"title": title, 'creationDate': creationDate, 'culture': culture, 'creators': authorList}))
 
 print(recordedList)
 
+# creating a connection to the Database created
+con = sl.connect('userprofiles.db')
 
+# creating a table within the database
+with con:
+    con.execute("""
+        CREATE TABLE ARTINFO (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            creation_date INTEGER, 
+            culture TEXT,
+            author TEXT,
+            biography TEXT
+        );       
+    """)
+sql = 'INSERT INTO ARTINFO (id, title, creation_date, culture, author, biography) values(?, ?, ?, ?, ?, ?)'
+
+with con:
+    data = con.execute("SELECT * FROM ARTINFO")
+    for row in data:
+        print(row)
