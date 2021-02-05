ace51cde251e15a2d6b1dcc722cd588668a8971b
ridhimainukurti <45546576+ridhimainukurti@users.noreply.github.com>
2021-02-05 12:47:58 -0800

    updated

--- artpiece.py
+++ artpiece.py
@@ -101,34 +101,3 @@ artInfos = ArtInfo.query.all()
 print(artInfos)
 
 
-#should create the table using the sqlalchemy library instead of sqlite3 library
-#class ArtInfo(db.Model):
-    #title = db.Column(db.String, nullable=False)
-    #creation_date = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    #culture = db.Column(db.String, nullable=False)
-    #author = db.Column(db.String, nullable=False)
-    #biography = db.Column(db.String, nullable=False)
-
-
-
-# creating a connection to the Database created
-#con = sl.connect('userprofiles.db')
-
-# creating a table within the database
-#with con:
-#    con.execute("""
-#        CREATE TABLE ARTINFO (
-#            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-#           title TEXT,
-#            creation_date INTEGER,
-#            culture TEXT,
-#            author TEXT,
-#            biography TEXT
-#        );
-#    """)
-#sql = 'INSERT INTO ARTINFO (id, title, creation_date, culture, author, biography) values(?, ?, ?, ?, ?, ?)'
-
-#with con:
-#    data = con.execute("SELECT * FROM ARTINFO")
-#    for row in data:
-#        print(row)
